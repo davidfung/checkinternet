5c8dec9ff9b4725a421e20a95424d6ed6910b389
davidfung <davidfung@amgcomputing.com>
2023-01-07 18:24:21 -0800

    First release

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
+use chrono::{DateTime, Local};
+use online::check;
+use std::{thread, time};
+
+const CHECK_INTERVAL: u64 = 5;
+
 fn main() {
-    use online::check;
-    println!("Online? {}", check(None).is_ok());
+    loop {
+        if check(None).is_ok() {
+            if false {
+                let now: DateTime<Local> = Local::now();
+                println!("Online @ {}", now);
+            }
+        } else {
+            let now: DateTime<Local> = Local::now();
+            println!("Offline @ {}", now);
+        }
+        let interval = time::Duration::from_secs(CHECK_INTERVAL);
+        thread::sleep(interval);
+    }
 }
